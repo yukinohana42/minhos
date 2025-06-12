8b287c87e587095897b6448a61f946db2335910e
yukinohana42 <yukinohana27@gmail.com>
2025-06-12 13:45:38 +0900

    Add files via upload

---
+++ online-script.js
@@ -0,0 +1,7 @@
+// indexonline.html のためのJavaScriptファイルです。
+// 今後、動的な機能を追加する際にこのファイルを使用します。
+
+document.addEventListener('DOMContentLoaded', () => {
+  console.log('indexonline.htmlページの準備ができました。');
+  // ここにコードを書いていくことで、様々な機能を追加できます。
+});
